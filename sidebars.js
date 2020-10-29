05597d46bf795bf0095e4c5464a2b6486a34a05c
crrmacarse <crrmacarse@gmail.com>
2020-10-30 01:36:38 +0800

    feat: added api; changed docs to projects;

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    'XtendOps Careers': ['doc1', 'doc2', 'doc3'],
+    'XConnect': ['mdx'],
+    'iFix': ['mdx'],
   },
 };
