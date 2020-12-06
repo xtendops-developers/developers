5bb219fee00116f73101787f5e47fcbcffcd6d9e
crrmacarse <christian@xtendops.us>
2020-12-06 16:25:11 +0800

    chore: updated deploy process

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo
 echo A product by the XtendOps DEV Team
-echo https://developers.xtendops.us
+echo https://developers.xtendops.com
 echo
 
-# pull latest changes
+git checkout .
+git checkout master
 git pull
 
 # install dependencies
@@ -12,6 +13,3 @@ npm install
 
 # build
 npm run build
-
-# restart pm2
-pm2 restart developers
